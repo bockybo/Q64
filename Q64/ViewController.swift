e8711fa57bcfb670285bbf7508e9a91ac48c7b31
evanchakrabarti <evanchakrabarti@gmail.com>
2023-01-18 16:16:04 -0500

    Initial Commit

---
+++ Q64/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Q64
+//
+//  Created by Evan Chakrabarti on 1/18/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
