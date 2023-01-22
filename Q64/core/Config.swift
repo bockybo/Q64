9207c91902f7bfdf0269a41ab09207d61fe19e8d
evanchakrabarti <evanchakrabarti@gmail.com>
2023-01-22 18:18:36 -0500

    org

--- Q64/core/Config.swift
+++ Q64/core/Config.swift
@@ -9,9 +9,6 @@ class Config {
 	static let color_fmt: MTLPixelFormat = .bgra8Unorm
 	static let depth_fmt: MTLPixelFormat = .depth32Float
 	
-	static let vtxfn = "vtx_main"
-	static let frgfn = "frg_main"
-	
 	static let fov: f32 = 75 * .pi/180
 	static let z0: f32 = 0.1
 	static let z1: f32 = 2000
