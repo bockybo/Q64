5b5ed05f807a2c7b9d7bfef2f888233394312796
evanchakrabarti <evanchakrabarti@gmail.com>
2023-01-24 03:22:22 -0500

    delete buffer

--- Q64/graphics/Light.swift
+++ Q64/graphics/Light.swift
@@ -9,3 +9,33 @@ struct Light {
 		self.hue = hue * amp
 	}
 }
+
+class Lights: Renderable {
+	static let maxnlt = 64
+	
+	let buf: MTLBuffer
+	var num = 0
+	init() {
+		self.buf = lib.device.makeBuffer(
+			length: Lights.maxnlt * util.sizeof(Light.self),
+			options: [.storageModeShared])!
+	}
+	
+	var ptr: UnsafeMutablePointer<Light> {
+		return self.buf.contents().assumingMemoryBound(to: Light.self)
+	}
+	func add(_ light: Light) {
+		self[self.num] = light
+		self.num += 1
+	}
+	subscript(i: Int) -> Light {
+		get {return self.ptr[i]}
+		set(light) {self.ptr[i] = light}
+	}
+	
+	func render(enc: MTLRenderCommandEncoder) {
+		enc.setFragmentBuffer(self.buf, offset: 0, index: 3)
+	}
+	
+}
+
